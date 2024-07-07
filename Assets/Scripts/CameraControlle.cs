9c9757f649ef19cdee7f3f707222f523f706ab6d
onurcgokmen <132705662+onurcgokmen@users.noreply.github.com>
2024-07-07 20:39:03 +0300

    ..

--- Assets/Scripts/CameraControlle.cs
+++ Assets/Scripts/CameraControlle.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CameraControlle : MonoBehaviour
 {
+    public GameObject player;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - player.transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
